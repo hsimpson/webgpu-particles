b0f8eb97bc280abd4968e2bf26807cf411e32593
Daniel Toplak <homer.simpson123@gmx.de>
2021-03-31 02:07:35 +0200

    refactor: React types

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './style.css';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
