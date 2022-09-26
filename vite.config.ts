10dca0c66624a199407b5db74f42bf7de001185f
Daniel Toplak <homer.simpson123@gmx.de>
2022-09-26 21:58:01 +0200

    feat: update packages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8081,
+    port: 8082,
+    host: '0.0.0.0',
   },
   build: {
     minify: true,
